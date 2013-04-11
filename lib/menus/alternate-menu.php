f47cec1fafa3afebd1e1f9e344e04f3175710feb
Nick Croft <NicktheGeek@NickGeek.com>
2013-04-11 17:52:09 -0400

    Completed alternate menu core
    small CSS fix so the alternate menu may inherit theme CSS but the main
    menus are still hidden when using fail safe CSS. Completed the menu
    import for the alternate menu.

--- lib/menus/alternate-menu.php
+++ lib/menus/alternate-menu.php
@@ -12,12 +12,12 @@ function gmm_alternate_menu( $menu_position ){
 	switch( $menu_position ){
 		case 'gmm_primary' :
 			add_filter( 'genesis_do_nav'   , 'gmm_do_mobile_alternate_nav', 99 , 3 );
-			add_filter( 'genesis_do_subnav', '_return_false'              , 99     );
+			add_filter( 'genesis_do_subnav', '__return_false'              , 99     );
 			
 			break;
 		case 'gmm_secondary' :
 			add_filter( 'genesis_do_subnav', 'gmm_do_mobile_alternate_subnav', 99, 3 );
-			add_filter( 'genesis_do_nav'   , '_return_false'                 , 99    );
+			add_filter( 'genesis_do_nav'   , '__return_false'                 , 99    );
 			
 			break;
 	}
@@ -30,12 +30,12 @@ function gmm_do_mobile_alternate_nav( $nav_output, $nav, $args ) {
 	$args = array(
 		'theme_location' => 'gmm-mobile-menu',	
 		'container'      => '', 
-		'menu_class'     => 'menu mobile',			
+		'menu_class'     => 'menu mobile gmm-menu gmm-alternate genesis-nav-menu menu-primary',			
 		'echo' => 0
 	);
 	
-	$mobile_menu = wp_nav_menu( $args );
-	$mobile_nav  = sprintf ( '<div class="gmm-menu gmm-alternate">%s</div>', $mobile_menu );
+	$mobile_nav = wp_nav_menu( $args );
+	//$mobile_nav  = sprintf ( '<div class="gmm-menu gmm-alternate">%s</div>', $mobile_menu );
 	$nav         = genesis_get_option( 'gmm_fail_safe' ) ? $nav : '';
 	$pattern     = genesis_markup( '<nav class="primary">%3$s%2$s%1$s%4$s</nav>', '<div id="nav">%3$s%2$s%1$s%4$s</div>', 0 );
 	
@@ -55,12 +55,12 @@ function gmm_do_mobile_alternate_subnav() {
 	$args = array(
 		'theme_location' => 'gmm-mobile-menu',	
 		'container'      => '', 
-		'menu_class'     => 'menu mobile',			
+		'menu_class'     => 'menu mobile gmm-menu gmm-alternate genesis-nav-menu menu-secondary',			
 		'echo' => 0
 	);
 	
-	$mobile_menu = wp_nav_menu( $args );
-	$mobile_nav  = sprintf ( '<div class="gmm-menu gmm-alternate">%s</div>', $mobile_menu );
+	$mobile_nav = wp_nav_menu( $args );
+	//$mobile_nav  = sprintf ( '<div class="gmm-menu gmm-alternate">%s</div>', $mobile_menu );
 	$nav         = genesis_get_option( 'gmm_fail_safe' ) ? $nav : '';
 	$pattern     = genesis_markup( '<nav class="secondary">%3$s%2$s%1$s%4$s</nav>', '<div id="subnav">%3$s%2$s%1$s%4$s</div>', 0 );
 	
